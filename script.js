8f6f6991539c8dcdac714d2587f83fb61cfb36b6
Sander van Zaane <mail@sandervanzaane.nl>
2020-12-01 17:01:44 +0100

    initial

---
+++ script.js
@@ -0,0 +1,18 @@
+// pseudocode
+
+// getData fucntie
+// sortByFirstName
+
+// button clicked --> eventlistner if clicked 
+
+// Find matchess
+
+// import { randomPersonData } from '.randomPersonData/randomPersonData.js'
+
+//import { randomPersonData } from './randomPersonData/randomPersonData.js';
+
+const getData = async() => {
+    const data = await randomPersonData();
+    console.log(data);
+};
+getData();
