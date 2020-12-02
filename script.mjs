75de769aed93657f251e00041322be3c5850f171
Sander van Zaane <mail@sandervanzaane.nl>
2020-12-02 10:53:34 +0100

    Add function that sorts array by first name

--- script.mjs
+++ script.mjs
@@ -1,3 +1,7 @@
 import randomPersonArr from "./randomPersonData/persons.mjs"
 
+const sortPersonsByFirstName = arr => arr.sort((a, b) => a.name < b.name ? -1 : 1);
+console.log(sortPersonsByFirstName(randomPersonArr));
+
+
 // const addListOnClick = document.getElementById("matchMaking").addEventListener("click", function showSorted() { console.log(randomPersonData) });
