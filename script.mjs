0d309b6d077d11bff3ed9f89ad8cdc8bc3d8034d
Sander van Zaane <mail@sandervanzaane.nl>
2020-12-02 11:30:33 +0100

    add sorting function

--- script.mjs
+++ script.mjs
@@ -1,7 +1,25 @@
 import randomPersonArr from "./randomPersonData/persons.mjs"
 
+// sort by firstname
 const sortPersonsByFirstName = arr => arr.sort((a, b) => a.name < b.name ? -1 : 1);
-console.log(sortPersonsByFirstName(randomPersonArr));
+//console.log(sortPersonsByFirstName(randomPersonArr));
+
+const sorted = sortPersonsByFirstName(randomPersonArr);
+
+// filter age >18
+
+const filterPersonsByAge = arr => arr.filter(obj => obj.age >= 18);
+const filtered = filterPersonsByAge(sorted);
+
+console.log(filtered);
+
+// const result = randomPersonArr
+//     .sort((a, b) => a.name < b.name ? -1 : 1)
+//     .filter(obj => obj.age >= 18);
+
+// console.log(result)
 
 
-// const addListOnClick = document.getElementById("matchMaking").addEventListener("click", function showSorted() { console.log(randomPersonData) });
+
+
+// const addListOnClick = document.getElementById("matchMaking").addEventListener("click", function showSorted()
