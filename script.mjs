88332be22a55e13b88d0d7c211a8ef03cad269fb
Sander van Zaane <mail@sandervanzaane.nl>
2020-12-02 12:17:56 +0100

    Add function that calculates correct zodiac name, output in html

--- script.mjs
+++ script.mjs
@@ -21,16 +21,21 @@ console.log(filtered);
 
 // add to dom
 
+const getZodiacNameFromBirthday = birthday => {
+    const date = new Date(birthday.mdy)
+    const signs = ['Aries: The Ram', 'Taurus: The Bull', 'Gemini: The Twins', 'Cancer: The Crab', 'Leo: The Lion', 'Virgo: The Virgin', 'Libra: The Scales', 'Scorpio: The Scorpion', 'Sagittarius: The Archer', 'Capricorn: The Goat', 'Aquarius: The Water Bearer', 'Pisces: The Fish'];
+    const sign = Number(new Intl.DateTimeFormat('fr-TN-u-ca-persian', { month: 'numeric' }).format(date) - 1);
+    return signs[sign]
+}
+
 filtered.forEach(person => {
     const ul = document.getElementById("matchmaking");
     const li = document.createElement("li");
-    const textHolder = document.createTextNode(`${person.name} ${person.surname}, ${person.age}, ${person.region} `);
+    const zodiac = getZodiacNameFromBirthday(person.birthday);
+    const textHolder = document.createTextNode(`${person.name} ${person.surname}, ${person.age}, ${person.region} ${zodiac}`);
     const img = document.createElement(`img`);
     img.src = person.photo;
     ul.appendChild(li);
     li.appendChild(textHolder);
     li.appendChild(img);
 });
-
-
-// const addListOnClick = document.getElementById("matchMaking").addEventListener("click", function showSorted()
